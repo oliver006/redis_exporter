813a24340b7f59deaff0f042b0afcefaa6ef55ba
Stan Hu <stanhu@gmail.com>
2018-03-19 20:59:35 -0700

    Move references of https://github.com/garyburd/redigo -> https://github.com/gomodule/redigo
    Bump redigo to 2.0.0 for new paths

--- exporter/redis_test.go
+++ exporter/redis_test.go
@@ -20,7 +20,7 @@ import (
 
 	"bytes"
 	"flag"
-	"github.com/garyburd/redigo/redis"
+	"github.com/gomodule/redigo/redis"
 	"github.com/prometheus/client_golang/prometheus"
 	dto "github.com/prometheus/client_model/go"
 )
