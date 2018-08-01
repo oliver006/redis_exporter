575b63788a347fb7cc5afc129f723254a5ffa81d
Oliver <oliver.hardt@nytimes.com>
2018-07-31 21:43:55 -0400

    try to fix CircleCI and make it build

--- build.sh
+++ build.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
 
+set -x
+
+echo "oliver"
+
 export CGO_ENABLED=0
+export GO_LDFLAGS="-extldflags \"-static\" -X main.VERSION=$CIRCLE_TAG -X main.COMMIT_SHA1=$CIRCLE_SHA1 -X main.BUILD_DATE=$(date +%F-%T)"
+
+go get github.com/mitchellh/gox
+go get github.com/tcnksm/ghr
 
 gox --osarch="linux/386"   -ldflags "$GO_LDFLAGS" -output "dist/redis_exporter"
+
+if [ ! -f dist/redis_exporter ]; then
+    echo "binary not found!"
+    exit 1
+fi
 
 echo "Build Docker images"
 
@@ -28,7 +41,7 @@ docker push "oliver006/redis_exporter:$CIRCLE_TAG"
 
 echo "Building binaries"
 echo ""
-echo $GO_LDFLAGS
+echo "GO_LDFLAGS: $GO_LDFLAGS"
 
 gox -rebuild --osarch="darwin/amd64"  -ldflags "$GO_LDFLAGS" -output "dist/redis_exporter" && cd dist && tar -cvzf redis_exporter-$CIRCLE_TAG.darwin-amd64.tar.gz redis_exporter && rm redis_exporter && cd ..
 gox -rebuild --osarch="darwin/386"    -ldflags "$GO_LDFLAGS" -output "dist/redis_exporter" && cd dist && tar -cvzf redis_exporter-$CIRCLE_TAG.darwin-386.tar.gz   redis_exporter && rm redis_exporter && cd ..
