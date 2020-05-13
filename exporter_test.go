34c779deb5f9a1e023166b5cf3717d53cdc97053
765PRO.P <gf940312@mail.ustc.edu.cn>
2020-05-13 09:40:58 +0800

    Fix slave_info missing label values (#383)
    * Fix slave_info missing label values
    
    * Add test about master_host and master_port

--- exporter_test.go
+++ exporter_test.go
@@ -19,6 +19,7 @@ import (
 	"net/url"
 	"os"
 	"reflect"
+	"regexp"
 	"sort"
 	"strings"
 	"sync"
@@ -1323,6 +1324,18 @@ func TestClusterSlave(t *testing.T) {
 	} {
 		if !strings.Contains(body, want) {
 			t.Errorf("Did not find key [%s] \nbody: %s", want, body)
+		}
+	}
+	hostReg, _ := regexp.Compile(`master_host="([0,1]?\d{1,2}|2([0-4][0-9]|5[0-5]))(\.([0,1]?\d{1,2}|2([0-4][0-9]|5[0-5]))){3}"`)
+	masterHost := hostReg.FindString(string(body))
+	portReg, _ := regexp.Compile(`master_port="(\d+)"`)
+	masterPort := portReg.FindString(string(body))
+	for wantedKey, wantedVal := range map[string]int{
+		masterHost: 5,
+		masterPort: 5,
+	} {
+		if res := strings.Count(body, wantedKey); res != wantedVal {
+			t.Errorf("Result: %s -> %d, Wanted: %d \nbody: %s", wantedKey, res, wantedVal, body)
 		}
 	}
 }
