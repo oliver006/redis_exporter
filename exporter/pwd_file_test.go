7aa851ce8a2c46947fb4720b2886a1e13beb752c
AnShen <shen.an89@gmail.com>
2020-12-25 15:26:35 +0800

    disable log password, and just log found or not found.

--- exporter/pwd_file_test.go
+++ exporter/pwd_file_test.go
@@ -1,19 +1,28 @@
 package exporter
 
 import (
-	"fmt"
+	"log"
 	"testing"
 )
 
 func TestLoadPwdFile(t *testing.T) {
-	confFile := "../sample-pwd-file.json"
-	p := NewPasswordMap()
-	err := p.LoadPwdFile(confFile)
+	confFile := "../contrib/sample-pwd-file.json"
+	passwordMap, err := LoadPwdFile(confFile)
 	if err != nil {
 		t.Fatalf("Test Failed, error: %v", err)
 	}
 
-	fmt.Println(len(p.RedisPwd))
-	fmt.Println("192.168.1.1 pwd is:", p.RedisPwd["192.168.1.1"])
-	fmt.Println("192.168.1.11 pwd is:", p.RedisPwd["192.168.1.11"])
+	redisHostList := []string{
+		"redis://192.168.1.1",
+		// Make sure it does not exist in sample-pwd-file.json
+		"redis://192.168.1.11",
+	}
+
+	for _, v := range redisHostList {
+		if passwordMap[v] != "" {
+			log.Printf("%s password found", v)
+		} else {
+			log.Printf("%s password Not found", v)
+		}
+	}
 }
