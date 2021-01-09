8ed3b9c432ee131b751b521804b30035044dc27e
AnShen <shen.an89@gmail.com>
2021-01-09 11:28:08 +0800

    添加文件读取测试

--- exporter/pwd_file_test.go
+++ exporter/pwd_file_test.go
@@ -1,11 +1,34 @@
 package exporter
 
 import (
+	"os"
 	"strings"
 	"testing"
 )
 
 func TestLoadPwdFile(t *testing.T) {
+	for _, tst := range []struct {
+		name    string
+		pwdFile string
+		want    error
+	}{
+		{name: "load-password-file-success", pwdFile: "../contrib/sample-pwd-file.json"},
+		{name: "load-password-file-failed", pwdFile: "non-existent.json"},
+	} {
+		_, tst.want = os.Open(tst.pwdFile)
+		t.Run(tst.name, func(t *testing.T) {
+			_, err := LoadPwdFile(tst.pwdFile)
+			if err == nil && err != tst.want {
+				t.Fatalf("Test Failed, result is not what we want")
+			}
+			if err != nil && err.Error() != tst.want.Error() {
+				t.Fatalf("Test Failed, result is not what we want")
+			}
+		})
+	}
+}
+
+func TestPasswordMap(t *testing.T) {
 	pwdFile := "../contrib/sample-pwd-file.json"
 	passwordMap, err := LoadPwdFile(pwdFile)
 	if err != nil {
