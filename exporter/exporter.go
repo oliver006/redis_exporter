2c968d5492a492e5f794ae35867878bba99b17aa
Nicolai Antiferov <75987872+nantiferov@users.noreply.github.com>
2024-05-30 18:38:13 +0300

    Feature: Add option to exclude consumer from Stream metrics (#906)
    * Feature: Add option to exclude consumer from Stream metrics

--- exporter/exporter.go
+++ exporter/exporter.go
@@ -46,40 +46,41 @@ type Exporter struct {
 }
 
 type Options struct {
-	User                      string
-	Password                  string
-	Namespace                 string
-	PasswordMap               map[string]string
-	ConfigCommandName         string
-	CheckKeys                 string
-	CheckSingleKeys           string
-	CheckStreams              string
-	CheckSingleStreams        string
-	CheckKeysBatchSize        int64
-	CheckKeyGroups            string
-	MaxDistinctKeyGroups      int64
-	CountKeys                 string
-	LuaScript                 map[string][]byte
-	ClientCertFile            string
-	ClientKeyFile             string
-	CaCertFile                string
-	InclConfigMetrics         bool
-	DisableExportingKeyValues bool
-	RedactConfigMetrics       bool
-	InclSystemMetrics         bool
-	SkipTLSVerification       bool
-	SetClientName             bool
-	IsTile38                  bool
-	IsCluster                 bool
-	ExportClientList          bool
-	ExportClientsInclPort     bool
-	ConnectionTimeouts        time.Duration
-	MetricsPath               string
-	RedisMetricsOnly          bool
-	PingOnConnect             bool
-	RedisPwdFile              string
-	Registry                  *prometheus.Registry
-	BuildInfo                 BuildInfo
+	User                          string
+	Password                      string
+	Namespace                     string
+	PasswordMap                   map[string]string
+	ConfigCommandName             string
+	CheckKeys                     string
+	CheckSingleKeys               string
+	CheckStreams                  string
+	CheckSingleStreams            string
+	StreamsExcludeConsumerMetrics bool
+	CheckKeysBatchSize            int64
+	CheckKeyGroups                string
+	MaxDistinctKeyGroups          int64
+	CountKeys                     string
+	LuaScript                     map[string][]byte
+	ClientCertFile                string
+	ClientKeyFile                 string
+	CaCertFile                    string
+	InclConfigMetrics             bool
+	DisableExportingKeyValues     bool
+	RedactConfigMetrics           bool
+	InclSystemMetrics             bool
+	SkipTLSVerification           bool
+	SetClientName                 bool
+	IsTile38                      bool
+	IsCluster                     bool
+	ExportClientList              bool
+	ExportClientsInclPort         bool
+	ConnectionTimeouts            time.Duration
+	MetricsPath                   string
+	RedisMetricsOnly              bool
+	PingOnConnect                 bool
+	RedisPwdFile                  string
+	Registry                      *prometheus.Registry
+	BuildInfo                     BuildInfo
 }
 
 // NewRedisExporter returns a new exporter of Redis metrics.
