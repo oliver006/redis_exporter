94011b95266d46dab696643250315d73b062cb71
joernchen <joernchen@phenoelit.de>
2024-12-11 13:57:04 +0100

    Remove empty line

--- exporter/http.go
+++ exporter/http.go
@@ -14,7 +14,6 @@ import (
 )
 
 func (e *Exporter) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-
 	err := e.verifyBasicAuth(r.BasicAuth())
 	if err != nil {
 		w.Header().Set("WWW-Authenticate", `Basic realm="redis-exporter, charset=UTF-8"`)
