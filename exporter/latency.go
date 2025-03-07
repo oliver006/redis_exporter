b6015d582ca3132fd42f01a310dafc609725451a
Oliver <oliver006@users.noreply.github.com>
2025-03-06 21:06:31 -0800

    Update a few deps (#982)
    * eqalpha/keydb:x86_64_v6.3.4
    
    * golang 1.24
    
    * github.com/prometheus/client_golang v1.21.1
    
    * golangci-lint 1.64
    
    * upgrade to alpine:3.21 for exporter docker image
    
    * pin docker images
    
    * cleanup

--- exporter/latency.go
+++ exporter/latency.go
@@ -40,8 +40,8 @@ func (e *Exporter) extractLatencyLatestMetrics(outChan chan<- prometheus.Metric,
 	for _, l := range reply {
 		if latencyResult, err := redis.Values(l, nil); err == nil {
 			var eventName string
-			var spikeLast, spikeDuration, max int64
-			if _, err := redis.Scan(latencyResult, &eventName, &spikeLast, &spikeDuration, &max); err == nil {
+			var spikeLast, spikeDuration, maxLatency int64
+			if _, err := redis.Scan(latencyResult, &eventName, &spikeLast, &spikeDuration, &maxLatency); err == nil {
 				spikeDurationSeconds := float64(spikeDuration) / 1e3
 				e.registerConstMetricGauge(outChan, "latency_spike_last", float64(spikeLast), eventName)
 				e.registerConstMetricGauge(outChan, "latency_spike_duration_seconds", spikeDurationSeconds, eventName)
