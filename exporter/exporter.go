d58dc6096fc7be5eefbd8dc629472be0441f3ba0
Stepan <51859698+co-cy@users.noreply.github.com>
2026-03-25 02:37:40 -0300

    feat: add --disable-scrape-endpoint flag to prevent SSRF credential l… (#1106)
    * feat: add --disable-scrape-endpoint flag to prevent SSRF credential leakage

--- exporter/exporter.go
+++ exporter/exporter.go
@@ -96,6 +96,7 @@ type Options struct {
 	SkipCheckKeysForRoleMaster     bool
 	InclMetricsForEmptyDatabases   bool
 	AppendInstanceRoleLabel        bool
+	DisableScrapeEndpoint          bool
 }
 
 func getInstanceRoleFromInfo(info string) string {
@@ -638,7 +639,9 @@ func NewRedisExporter(uri string, opts Options) (*Exporter, error) {
 	}
 
 	e.mux.HandleFunc("/", e.indexHandler)
-	e.mux.HandleFunc("/scrape", e.scrapeHandler)
+	if !e.options.DisableScrapeEndpoint {
+		e.mux.HandleFunc("/scrape", e.scrapeHandler)
+	}
 	e.mux.HandleFunc("/discover-cluster-nodes", e.discoverClusterNodesHandler)
 	e.mux.HandleFunc("/health", e.healthHandler)
 	e.mux.HandleFunc("/-/reload", e.reloadPwdFile)
